77cd0fa0769066ff5247f880c80b9efd70776218
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-08-23 19:59:15 -0400

    (feat): Added class-level custom validation

--- WebApiDeepDive/CourseLibrary.API/Models/CourseForUpdateDto.cs
+++ WebApiDeepDive/CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -1,9 +1,10 @@
-﻿namespace CourseLibrary.API.Models
-{
-    public class CourseForUpdateDto
-    {
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace CourseLibrary.API.Models
+{
+    public class CourseForUpdateDto : CourseForManipulationDto
+    {
+        [Required(ErrorMessage = "The description field is required.")]
+        public override string Description { get => base.Description; set => base.Description = value; }
     }
 }
